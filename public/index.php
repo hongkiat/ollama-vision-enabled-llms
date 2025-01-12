60f015e260bc83120dbe1eefd58a39a09dd099ae
Thoriq Firdaus <2067467+tfirdaus@users.noreply.github.com>
2025-01-13 00:10:43 +0700

    Uncomment `AltText`

--- public/index.php
+++ public/index.php
@@ -7,12 +7,12 @@ use Hongkiat\Ollama\VisionEnabled\VisualTesting;
 require dirname(__DIR__) . '/vendor/autoload.php';
 
 // 1. Image-to-Text Generation.
-// $response = (new AltText())->fromImage(dirname(__DIR__) . '/img/image-1.jpg');
+$response = (new AltText())->fromImage(dirname(__DIR__) . '/img/image-1.jpg');
 
 // 2. Visual Data Extraction
 // $response = (new TableExtractor())->fromImage(dirname(__DIR__) . '/img/image-2.jpg');
 
 // 3. Visual Testing
-$response = (new VisualTesting())->fromImage(dirname(__DIR__) . '/img/image-3b.jpg');
+// $response = (new VisualTesting())->fromImage(dirname(__DIR__) . '/img/image-3b.jpg');
 
 echo (new Parsedown())->text($response);
